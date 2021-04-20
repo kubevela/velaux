b7233bff0e53a4b2daee872542f59a3c1a327d05
Hongchao Deng <hongchaodeng1@gmail.com>
2021-04-19 21:59:43 -0400

    fix breadcrumb

--- ui/src/pages/Application/Detail/index.tsx
+++ ui/src/pages/Application/Detail/index.tsx
@@ -18,7 +18,7 @@ export default (props: any) => {
           routes: [
             {
               path: '',
-              breadcrumbName: 'Clusters',
+              breadcrumbName: 'Applications',
             },
             {
               path: '',
