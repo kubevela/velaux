d6ecddb6a68b37598f782d7a955aeb7a0e690ac5
barnettZQG <yiyun.pro>
2021-10-13 16:54:56 +0800

    Fix: delete Translation

--- src/components/Translation/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-
-const Translation = ({ children, className = '' }) => {
-  const { t } = useTranslation();
-  return <span className={className}>{t(children)}</span>;
-};
-
-export default Translation;
