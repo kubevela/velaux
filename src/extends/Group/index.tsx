11d3315e6ceb394c53efd245170edb34d8da634c
wangbow <18700441876@126.com>
2021-12-15 16:30:02 +0800

    Fix: #262 ui schema switch change, the configuration optimization (#272)
    * Fix: #262 ui schema switch change, the configuration optimization
    
    Signed-off-by: wb-wb665667 <wb-wb665667@alibaba-inc.com>
    
    * Fix: #262 ui schema switch change, the configuration optimization
    
    Signed-off-by: wb-wb665667 <wb-wb665667@alibaba-inc.com>
    
    Co-authored-by: wb-wb665667 <wb-wb665667@alibaba-inc.com>
    Co-authored-by: barnettZQG <barnett.zqg@gmail.com>

--- src/extends/Group/index.tsx
+++ src/extends/Group/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import { Icon, Loading, Grid, Switch } from '@b-design/ui';
+import { Icon, Loading, Grid, Switch, Field, Dialog } from '@b-design/ui';
 import { If } from 'tsx-control-statements/components';
+import Translation from '../../components/Translation';
+import locale from '../../utils/locale';
 import './index.less';
 
 const { Col, Row } = Grid;
-
 type Props = {
   title: string | React.ReactNode;
   description?: string | React.ReactNode;
@@ -14,11 +15,16 @@ type Props = {
   loading?: boolean;
   hasToggleIcon?: boolean;
   required?: boolean;
+  field?: Field;
+  jsonKey?: string;
+  propertyValue?: any;
+  onChange?: (values: any) => void;
 };
 
 type State = {
   closed: boolean | undefined;
   enable?: boolean;
+  checked: boolean;
 };
 
 class Group extends React.Component<Props, State> {
@@ -28,6 +34,7 @@ class Group extends React.Component<Props, State> {
     this.state = {
       closed: props.closed,
       enable: props.required,
+      checked: false,
     };
   }
 
@@ -37,9 +44,34 @@ class Group extends React.Component<Props, State> {
       closed: !closed,
     });
   };
+
+  componentDidMount() {
+    this.initSwitchState();
+  }
+
+  initSwitchState = () => {
+    const { jsonKey = '', propertyValue = {} } = this.props;
+    const findKey = Object.keys(propertyValue).find((item) => item === jsonKey);
+    if (findKey) {
+      this.setState({ enable: true, closed: false, checked: true });
+    } else {
+      this.setState({ enable: false, closed: false, checked: false });
+    }
+  };
+
+  removeJsonKeyValue() {
+    const { jsonKey = '', onChange } = this.props;
+    const field: Field | undefined = this.props.field;
+    if (field && onChange) {
+      field.remove(jsonKey);
+      const values = field.getValues();
+      onChange(values);
+    }
+  }
+
   render() {
     const { title, description, children, hasToggleIcon, loading, required } = this.props;
-    const { closed, enable } = this.state;
+    const { closed, enable, checked } = this.state;
     return (
       <Loading visible={loading || false} style={{ width: '100%' }}>
         <div className="group-container">
@@ -53,8 +85,27 @@ class Group extends React.Component<Props, State> {
                 <If condition={!required}>
                   <Switch
                     size="small"
+                    defaultChecked={required}
+                    checked={checked}
                     onChange={(event: boolean) => {
-                      this.setState({ enable: event, closed: false });
+                      if (event === true) {
+                        this.setState({ enable: event, closed: false, checked: true });
+                      } else if (event === false) {
+                        Dialog.confirm({
+                          type: 'confirm',
+                          content: (
+                            <Translation>
+                              If Swtich is turned off, The configuration will be reset. Are you sure
+                              you want to do this?
+                            </Translation>
+                          ),
+                          onOk: () => {
+                            this.setState({ enable: event, closed: false, checked: false });
+                            this.removeJsonKeyValue();
+                          },
+                          locale: locale.Dialog,
+                        });
+                      }
                     }}
                   />
                 </If>
