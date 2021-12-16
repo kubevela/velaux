b1b6b402562794d4c943de4ec71d617f4fd71f8a
wb-wb665667 <wb-wb665667@alibaba-inc.com>
2021-12-16 19:30:30 +0800

    Fix: uiSchema CMD enter optimization

--- src/extends/Strings/index.tsx
+++ src/extends/Strings/index.tsx
@@ -60,8 +60,8 @@ class Strings extends React.Component<Props, State> {
     super(props);
     const inputList: ListParams[] = [];
     if (props.value) {
-      props.value.map((v: any) => {
-        const key = Date.now().toString();
+      props.value.map((v: any,index:number) => {
+        const key = Date.now().toString() + index;
         inputList.push({
           key,
           value: v,
@@ -78,15 +78,23 @@ class Strings extends React.Component<Props, State> {
     });
   }
 
-  componentDidMount = async () => {};
+  componentDidMount = async () => { };
 
   changeValues = () => {
     const values = this.field.getValues();
-
-    const result = Object.keys(values)
-      .map((key) => values[key])
-      .filter((item) => !!item);
-    this.props.onChange(result);
+    const inputList: ListParams[]  = this.state.inputList;
+    Object.keys(values)
+      .map((key) => {
+         if(Array.isArray(inputList)){
+          inputList.forEach(item=> {
+            if(item.key === key){
+               item.value = values[key];
+            }
+          })
+         }
+      });
+    const valuesInfo = inputList.map((item:ListParams) => item.value)
+    this.props.onChange(valuesInfo);
   };
 
   addInputItem = () => {
