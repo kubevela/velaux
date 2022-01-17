e8d0572aea2a0a00ba28ff183c8aede2c0ea9a0e
barnettZQG <barnett.zqg@gmail.com>
2022-01-17 14:30:23 +0800

    Fix: the api path is invalid
    Signed-off-by: barnettZQG <barnett.zqg@gmail.com>

--- src/api/config.ts
+++ src/api/config.ts
@@ -4,12 +4,12 @@ const domainObj = getDomain();
 export const baseURL = domainObj.APIBASE || domainObj.MOCK;
 
 export function loadSystemInfo() {
-  const url = baseURL + `/api/v1/system_info`;
+  const url = `/api/v1/system_info`;
   return get(url, {});
 }
 
 export function updateSystemInfo(params: { enableCollection: boolean }) {
-  const url = baseURL + `/api/v1/system_info`;
+  const url = `/api/v1/system_info`;
   return put(url, {
     enableCollection: params.enableCollection,
   });
