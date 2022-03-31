1c5b5ab7f18dc772487d11829d2a936394e3d825
wangbow <18700441876@126.com>
2022-03-31 15:03:16 +0800

    Feat: login page add error message show (#413)
    * Feat: logon error apply form bottom
    
    Signed-off-by: wb-wb665667 <wb-wb665667@alibaba-inc.com>
    
    * Feat: login error message show
    
    Signed-off-by: wb-wb665667 <wb-wb665667@alibaba-inc.com>
    
    * Feat: login error add transtion
    
    Signed-off-by: wb-wb665667 <wb-wb665667@alibaba-inc.com>
    
    * Feat: login error code change
    
    Signed-off-by: wb-wb665667 <wb-wb665667@alibaba-inc.com>
    
    * Feat: zh.json update
    
    Signed-off-by: wb-wb665667 <wb-wb665667@alibaba-inc.com>
    
    * Fix: request error redundant process
    
    Signed-off-by: wb-wb665667 <wb-wb665667@alibaba-inc.com>
    
    Co-authored-by: wb-wb665667 <wb-wb665667@alibaba-inc.com>

--- src/pages/CallBackPage/index.tsx
+++ src/pages/CallBackPage/index.tsx
@@ -30,17 +30,19 @@ export default class CallBackPage extends React.Component<Props> {
   };
 
   onLogonSSO = (code: any) => {
-    loginSSO({ code }).then((res: any) => {
-      if (res && res.accessToken) {
-        localStorage.setItem('token', res.accessToken);
-        localStorage.setItem('refreshToken', res.refreshToken);
-        this.props.history.push('/');
-      } else if (res && res.data && res.data.BusinessCode) {
+    loginSSO({ code })
+      .then((res: any) => {
+        if (res && res.accessToken) {
+          localStorage.setItem('token', res.accessToken);
+          localStorage.setItem('refreshToken', res.refreshToken);
+          this.props.history.push('/');
+        }
+      })
+      .catch(() => {
         setTimeout(() => {
           this.props.history.push('/login');
         }, 3000);
-      }
-    });
+      });
   };
 
   render() {
