95483711618b04364a691d19cc4aa1c6e09114df
wb-wb665667 <wb-wb665667@alibaba-inc.com>
2022-04-12 11:03:55 +0800

    Fix: addon page update button permisson
    Signed-off-by: wb-wb665667 <wb-wb665667@alibaba-inc.com>

--- src/pages/Addons/index.tsx
+++ src/pages/Addons/index.tsx
@@ -103,7 +103,7 @@ class Addons extends React.Component<Props, State> {
           title="Addons"
           subTitle="Manages and extends platform capabilities"
           extButtons={[
-            <Permission request={{ resource: 'addonRegistry:*', action: 'create' }} project={''}>
+            <Permission request={{ resource: 'addonRegistry:*', action: 'list' }} project={''}>
               <Button
                 type="primary"
                 onClick={() => {
