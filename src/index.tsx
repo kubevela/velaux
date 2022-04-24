121d5989978807ee55f0ee7dfe95a5d3d02ea284
wb-wb665667 <wb-wb665667@alibaba-inc.com>
2022-04-24 14:19:16 +0800

    Fix: add b-design css
    Signed-off-by: wb-wb665667 <wb-wb665667@alibaba-inc.com>

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { createModel } from './store';
 import { createBrowserHistory } from 'history';
 import RouterView from './router';
 import './i18n';
-import './assets/dist/index.css';
+import './assets/dist/b-design.css';
 
 const app: any = dva({
   history: createBrowserHistory(),
