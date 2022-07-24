8d6b7b8a6645fb798058c6f10d07dc62f7822ca4
barnettZQG <barnett.zqg@gmail.com>
2022-07-24 14:24:43 +0800

    Feat: support to set the default roles
    Signed-off-by: barnettZQG <barnett.zqg@gmail.com>

--- src/components/PlatformSetting/index.tsx
+++ src/components/PlatformSetting/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'dva';
 import { routerRedux } from 'dva/router';
-import { Field, Grid, Radio, Input, Message, Icon, Switch } from '@b-design/ui';
+import { Field, Grid, Radio, Input, Message, Icon, Switch, Select } from '@b-design/ui';
 import { Dialog, Card, Button, Form } from '@b-design/ui';
 import Translation from '../../components/Translation';
 import locale from '../../utils/locale';
@@ -13,7 +13,11 @@ import { checkPermission } from '../../utils/permission';
 import Item from '../Item';
 import { If } from 'tsx-control-statements/components';
 import { getDexConfig } from '../../api/authentication';
-
+import { v4 as uuid } from 'uuid';
+import { AiOutlineDelete, AiOutlinePlus } from 'react-icons/ai';
+import { getProjectList, getProjectRoles } from '../../api/project';
+import type { Project, ProjectRoleBase } from '../../interface/project';
+import { CustomSelect } from '../CustomSelect';
 const { Col, Row } = Grid;
 
 type Props = {
@@ -28,6 +32,9 @@ type Props = {
 type State = {
   businessGuideCode: number;
   dexConfig?: DexConfig;
+  defaultProjectItems: { id: string; name?: string; roles?: string[] }[];
+  projects?: Project[];
+  roles?: ProjectRoleBase[];
 };
 
 type QuickShowRet = {
@@ -42,21 +49,34 @@ class PlatformSetting extends React.Component<Props, State> {
   dialogGuideUser: QuickShowRet | undefined;
   constructor(props: Props) {
     super(props);
-    this.field = new Field(this);
+    this.field = new Field(this, {
+      onChange: (name: string, value: any) => {
+        if (name.endsWith('-name')) {
+          this.loadProjectRoles(value);
+        }
+      },
+    });
     this.state = {
       businessGuideCode: 0,
+      defaultProjectItems: [],
     };
   }
 
   componentDidMount() {
+    this.listProjects();
+    this.onGetDexConfig();
     this.field.setValues({
       loginType: this.props.systemInfo.loginType,
       enableCollection: this.props.systemInfo.enableCollection,
     });
-    this.ontDexConfig();
+    const items: { id: string; name: string; roles: string[] }[] = [];
+    this.props.systemInfo.dexUserDefaultProjects?.map((item) => {
+      items.push({ id: uuid(), ...item });
+    });
+    this.setState({ defaultProjectItems: items });
   }
 
-  ontDexConfig = () => {
+  onGetDexConfig = () => {
     getDexConfig().then((res: DexConfig) => {
       if (res) {
         this.setState({
@@ -72,7 +92,40 @@ class PlatformSetting extends React.Component<Props, State> {
         return;
       }
       const { enableCollection, loginType, velaAddress } = values;
-      const param = { enableCollection, loginType, velaAddress };
+      const projectMaps: Record<
+        string,
+        {
+          name: string;
+          roles: string[];
+        }
+      > = {};
+      Object.keys(values).map((key) => {
+        if (key.endsWith('-name')) {
+          const infos = key.split('-');
+          if (!projectMaps[infos[0]]) {
+            projectMaps[infos[0]] = { name: values[key], roles: [] };
+          } else {
+            projectMaps[infos[0]].name = values[key];
+          }
+        }
+        if (key.endsWith('-roles')) {
+          const infos = key.split('-');
+          if (!projectMaps[infos[0]]) {
+            projectMaps[infos[0]] = { roles: values[key], name: '' };
+          } else {
+            projectMaps[infos[0]].roles = values[key];
+          }
+        }
+      });
+
+      const param = {
+        enableCollection,
+        loginType,
+        velaAddress,
+        dexUserDefaultProjects: Object.keys(projectMaps).map((key) => {
+          return projectMaps[key];
+        }),
+      };
       updateSystemInfo(param)
         .then((res) => {
           if (res) {
@@ -186,12 +239,60 @@ class PlatformSetting extends React.Component<Props, State> {
     )}/dex/auth?client_id=${clientID}&redirect_uri=${newRedirectURl}&response_type=code&scope=openid+profile+email+offline_access&state=velaux`;
   };
 
+  onAddItem = () => {
+    this.setState((prevState) => ({
+      defaultProjectItems: [...prevState.defaultProjectItems, { id: uuid() }],
+    }));
+  };
+
+  onRemoveItem = (id: string) => {
+    this.setState((prevState) => ({
+      defaultProjectItems: prevState.defaultProjectItems.filter((cp) => cp.id != id) || [],
+    }));
+    this.field.remove(`${id}-name`);
+    this.field.remove(`${id}-roles`);
+  };
+
   getIssuerDefaultValue = () => {
     const domain = `${window.location.protocol}//${window.location.host}`;
     return domain;
   };
+
+  generateProjectOptions = () => {
+    return this.state.projects?.map((item) => {
+      return {
+        label: `${item.name}(${item.alias || '-'})`,
+        value: item.name,
+      };
+    });
+  };
+
+  generateProjectRoleOptions = () => {
+    return this.state.roles?.map((item) => {
+      return {
+        label: `${item.name}(${item.alias || '-'})`,
+        value: item.name,
+      };
+    });
+  };
+
+  listProjects = async () => {
+    getProjectList({ page: 0, pageSize: 0 }).then((res) => {
+      this.setState({
+        projects: res.projects || [],
+      });
+    });
+  };
+
+  loadProjectRoles = async (name: string) => {
+    getProjectRoles({ projectName: name }).then((res) => {
+      this.setState({ roles: res?.roles || [] });
+    });
+  };
+
   render() {
     const { onClose, platformSetting, systemInfo } = this.props;
+    const { defaultProjectItems } = this.state;
     return (
       <Dialog
         locale={locale().Dialog}
@@ -256,6 +357,53 @@ class PlatformSetting extends React.Component<Props, State> {
                       rules: [{ required: true }],
                     })}
                   />
+                </Form.Item>
+              </Col>
+              <Col span={24}>
+                <Form.Item label={i18n.t('Default projects for the dex login user')}>
+                  <div className="flexright" style={{ marginBottom: '8px' }}>
+                    <Button onClick={this.onAddItem}>
+                      <AiOutlinePlus />
+                    </Button>
+                  </div>
+                  {defaultProjectItems.map((item) => {
+                    return (
+                      <Row id={item.id} key={item.id}>
+                        <Col span={8} style={{ padding: '0 4px' }}>
+                          <Form.Item labelAlign="inset" required={true} label={i18n.t('Project')}>
+                            <Select
+                              locale={locale().Select}
+                              {...this.field.init(`${item.id}-name`, {
+                                rules: [{ required: true }],
+                                initValue: item.name,
+                              })}
+                              dataSource={this.generateProjectOptions()}
+                            />
+                          </Form.Item>
+                        </Col>
+                        <Col span={12} style={{ padding: '0 4px' }}>
+                          <Form.Item labelAlign="inset" required={true} label={i18n.t('Roles')}>
+                            <CustomSelect
+                              locale={locale().Select}
+                              {...this.field.init(`${item.id}-roles`, {
+                                rules: [{ required: true }],
+                                initValue: item.roles,
+                              })}
+                              dataSource={this.generateProjectRoleOptions()}
+                              mode="multiple"
+                            />
+                          </Form.Item>
+                        </Col>
+                        <Col span={4}>
+                          <div className="flexright">
+                            <Button onClick={() => this.onRemoveItem(item.id)}>
+                              <AiOutlineDelete />
+                            </Button>
+                          </div>
+                        </Col>
+                      </Row>
+                    );
+                  })}
                 </Form.Item>
               </Col>
             </Row>
