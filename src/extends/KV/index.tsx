a132e4b072befed3d8b20932c96890b9a9acd7f5
weiy730 <weiy730@163.com>
2022-11-03 14:33:26 +0800

    Feat: add type selection in KV
    Signed-off-by: weiy730 <weiy730@163.com>
    
    Co-authored-by: barnettZQG <barnett.zqg@gmail.com>
    
    Signed-off-by: weiy730 <weiy730@163.com>
    
    Signed-off-by: weiy730 <weiy730@163.com>

--- src/extends/KV/index.tsx
+++ src/extends/KV/index.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Grid, Button, Icon, Form, Input, Field, Select } from '@b-design/ui';
+import type { Rule } from '@alifd/field';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import _ from 'lodash';
 import type { UIParam } from '../../interface/application';
@@ -80,6 +81,7 @@ class KV extends Component<Props, State> {
         });
         this.form.setValue('envKey-' + key, label);
         this.form.setValue('envValue-' + key, value[label]);
+        this.form.setValue('envType-' + key, this.getValueType(value[label]))
       }
     }
 
@@ -107,6 +109,7 @@ class KV extends Component<Props, State> {
         if (!item) {
           item = { key: '', label: '', valueType: 'string' };
         }
+        item.key = index
         item.label = values[key];
         items.set(index, item);
       }
@@ -115,16 +118,34 @@ class KV extends Component<Props, State> {
         const index = key.replace('envValue-', '');
         let item = items.get(index);
         if (!item) {
-          item = { key: '', label: '', valueType: 'string' };
+          item = { key: '', label: '', valueType: 'string', };
         }
         item.value = values[key];
         items.set(index, item);
       }
+      if (key.startsWith('envType-')) {
+        const index = key.replace('envType-', '');
+        let item = items.get(index);
+        if (!item) {
+          item = { key: '', label: '', valueType: '' };
+        }
+        item.valueType = values[key];
+        if (item.value == undefined) {
+          item.value = values['envValue-' + index]
+        }
+        if (this.getValueType(item.value) !== item.valueType && item.valueType !== 'object') {
+          item.value = item.valueType == 'boolean' ? true : ''
+        }
+        items.set(index, item);
+      }
     });
     const obj = Object.create(null);
+    const newItems: Item[] = []
     items.forEach((item: Item) => {
       obj[item.label] = item.value;
+      newItems.push(item)
     });
+    this.setState({ items: newItems });
     const { onChange } = this.props;
     if (onChange) {
       onChange(obj);
@@ -140,6 +161,7 @@ class KV extends Component<Props, State> {
     });
     this.form.remove('envKey-' + key);
     this.form.remove('envValue-' + key);
+    this.form.remove('envType-' + key);
     this.setState({ items: items });
     this.submit();
   }
@@ -150,7 +172,7 @@ class KV extends Component<Props, State> {
 
   getValueType = (value: any) => {
     const findValueType = this.matchOutSideValueType();
-    const valueTypeAdditionalParam = ['number', 'boolean'];
+    const valueTypeAdditionalParam = ['number', 'boolean', 'object'];
     if (valueTypeAdditionalParam.includes(findValueType)) {
       return findValueType;
     } else {
@@ -186,84 +208,150 @@ class KV extends Component<Props, State> {
     if (inputValue) {
       dataSource.push(inputValue);
     }
+    const options = [{ label: 'string', value: 'string' }, { label: 'number', value: 'number' }, { label: 'boolean', value: 'boolean' }, { label: 'object', value: 'object' }]
+    const checkObjectFormat = (rule: Rule, value: any, callback: (error?: string) => void) => {
+      if (value == undefined) {
+        callback()
+        return
+      }
+      if (isNaN(Number(value))) {
+        try {
+          if (typeof JSON.parse(value) == "object") {
+            callback()
+          }
+        } catch {
+          callback('Wrong format')
+        }
+      }
+      callback('Wrong format')
+    }
+
     return (
-      <div id={id}>
-        {items.map((item) => {
-          return (
-            <Row key={item.key} gutter="20">
-              <Col span={10}>
-                <Form.Item>
-                  <If condition={dataSource && dataSource.length > 0}>
-                    <Select
-                      showSearch={true}
-                      dataSource={dataSource}
-                      disabled={this.props.disabled}
-                      {...init(`envKey-${item.key}`)}
-                      label={'Key'}
-                      placeholder={i18n.t('Please select')}
-                      locale={locale().Select}
-                      onSearch={this.onSearch}
-                    />
-                  </If>
-                  <If condition={!keyOptions}>
-                    <Input
-                      disabled={this.props.disabled}
-                      {...init(`envKey-${item.key}`)}
-                      label={'Key'}
-                      className="full-width"
-                      placeholder={''}
-                    />
-                  </If>
-                </Form.Item>
-              </Col>
-              <Col span={10}>
-                <Form.Item>
-                  <If condition={item.valueType == 'number' || item.valueType == 'string'}>
-                    <Input
-                      disabled={this.props.disabled}
-                      htmlType={item.valueType == 'number' ? 'number' : ''}
-                      {...init(`envValue-${item.key}`)}
-                      label={'Value'}
-                      className="full-width"
-                      placeholder={i18n.t(
-                        item.valueType == 'number'
-                          ? 'Please input a number'
-                          : 'Please input a value',
-                      )}
-                    />
-                  </If>
-                  <If condition={item.valueType == 'boolean'}>
-                    <div style={{ display: 'flex', alignItems: 'center' }}>
-                      <span style={{ lineHeight: '36px', marginRight: '16px' }}>Value:</span>
-                      <Switch
-                        checked={this.form.getValue(`envValue-${item.key}`)}
-                        disabled={this.props.disabled}
-                        {...init(`envValue-${item.key}`)}
-                        className="full-width"
+      
+        <div id={id}>
+          {items.map((item) => {
+            return (
+              <Form field={this.form}>
+                <Row key={item.key} gutter="20">
+                  <Col span={8}>
+                    <Form.Item>
+                      <If condition={dataSource && dataSource.length > 0}>
+                        <Select
+                          showSearch={true}
+                          dataSource={[{ label: 'On Webhook Event', value: 'webhook' }]}
+                          disabled={this.props.disabled}
+                          {...init(`envKey-${item.key}`)}
+                          label={'Key'}
+                          placeholder={i18n.t('Please select')}
+                          locale={locale().Select}
+                          onSearch={this.onSearch}
+                        />
+                      </If>
+                      <If condition={!keyOptions}>
+                        <Input
+                          disabled={this.props.disabled}
+                          {...init(`envKey-${item.key}`)}
+                          label={'Key'}
+                          className="full-width"
+                          placeholder={''}
+                        />
+                      </If>
+                    </Form.Item>
+                  </Col>
+                  <Col span={6}>
+                    <Form.Item>
+                      <Select
+                        showSearch={true}
+                        dataSource={options}
+                        {...init(`envType-${item.key}`)}
+                        label={'Type'}
+                        placeholder={i18n.t('Please select')}
+                        locale={locale().Select}
+                        onSearch={this.onSearch}
+                        value={item.valueType}
+                        onChange={(v) => {
+                          this.form.setValue('envType-' + item.key, v)
+                          item.valueType = v
+                          if (v == 'boolean') {
+                            this.form.setValue('envValue-' + item.key, true)
+                            item.value = true
+                          } else {
+                            this.form.setValue('envValue-' + item.key, '')
+                            item.value = ''
+                          }
+                          this.setState({ items: items });
+                        }}
                       />
+                    </Form.Item>
+                  </Col>
+                  <Col span={9}>
+                    <Form.Item>
+                      <If condition={item.valueType == 'number' || item.valueType == 'string'}>
+                        <Input
+                          disabled={this.props.disabled}
+                          htmlType={item.valueType == 'number' ? 'number' : ''}
+                          {...init(`envValue-${item.key}`)}
+                          value={item.value}
+                          label={'Value'}
+                          className="full-width"
+                          placeholder={i18n.t(
+                            item.valueType == 'number'
+                              ? 'Please input a number'
+                              : 'Please input a value',
+                          )}
+                        />
+                      </If>
+                      <If condition={item.valueType == 'object'}>
+                        <Input
+                          name={`envValue-${item.key}`}
+                          disabled={this.props.disabled}
+                          label={'Value'}
+                          className="full-width"
+                          placeholder={i18n.t('Please input a object')}
+                          {...init(`envValue-${item.key}`, {
+                            rules: [
+                              {
+                                validator: checkObjectFormat,
+                                message: 'Wrong format.',
+                              },
+                            ],
+                          })}
+                          value={item.value}
+                        />
+                      </If>
+                      <If condition={item.valueType == 'boolean'}>
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                          <span style={{ lineHeight: '36px', marginRight: '16px' }}>Value:</span>
+                          <Switch
+                            checked={this.form.getValue(`envValue-${item.key}`)}
+                            disabled={this.props.disabled}
+                            {...init(`envValue-${item.key}`)}
+                            className="full-width"
+                          />
+                        </div>
+                      </If>
+                    </Form.Item>
+                  </Col>
+                  <Col span={1}>
+                    <div className="mt-5" style={{ padding: '8px 0' }}>
+                      <Icon type="ashbin" size="small" onClick={() => this.remove(item.key)} />
                     </div>
-                  </If>
-                </Form.Item>
-              </Col>
-              <Col span={1}>
-                <div className="mt-5" style={{ padding: '8px 0' }}>
-                  <Icon type="ashbin" size="small" onClick={() => this.remove(item.key)} />
-                </div>
-              </Col>
-            </Row>
-          );
-        })}
-        <div className="mb-20 flexright">
-          <Button
-            disabled={this.props.disabled}
-            size="small"
-            type="secondary"
-            onClick={this.addItem.bind(this)}
-          >
-            Add
-          </Button>
+                  </Col>
+                </Row>
+              </Form>
+            );
+          })}
+          <div className="mb-20 flexright">
+            <Button
+              disabled={this.props.disabled}
+              size="small"
+              type="secondary"
+              onClick={this.addItem.bind(this)}
+            >
+              Add
+            </Button>
+          </div>
         </div>
-      </div>
     );
   }
 }
