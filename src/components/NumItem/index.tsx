69f61382237cfa088e88a1f3701bc2f1c7023346
barnettZQG <barnett.zqg@gmail.com>
2023-01-31 11:32:12 +0800

    Feat: support for deleting the workflow
    Signed-off-by: barnettZQG <barnett.zqg@gmail.com>
    (cherry picked from commit 8bce563e17cf3439bc3644fca13fe95196c3581c)

--- src/components/NumItem/index.tsx
+++ src/components/NumItem/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './index.less';
 
 export type Props = {
   title: string | React.ReactNode;
   number?: number;
+  to?: string;
 };
 
 const NumItem: React.FC<Props> = (props) => {
   return (
     <div className="num-item">
-      <div className="number">{props.number || 0}</div>
+      <div className="number">
+        {props.to ? (
+          <Link to={props.to}>{props.number || 0}</Link>
+        ) : (
+          <span>{props.number || 0}</span>
+        )}
+      </div>
       <div className="title">{props.title}</div>
     </div>
   );
