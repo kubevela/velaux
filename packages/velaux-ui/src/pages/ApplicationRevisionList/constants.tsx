1092aaf8f34c683d121350fde3ed12beb5601088
jiaxiaolei <jiaxiaolei@cmss.chinamobile.com>
2023-03-13 20:11:57 +0800

    fix: Add the processing of rollback status to the version list
    Signed-off-by: jiaxiaolei <584736476@qq.com>

--- packages/velaux-ui/src/pages/ApplicationRevisionList/constants.tsx
+++ packages/velaux-ui/src/pages/ApplicationRevisionList/constants.tsx
@@ -4,4 +4,5 @@ export const statusList = [
   { label: 'Complete', value: 'complete' },
   { label: 'Failure', value: 'failure' },
   { label: 'Terminated', value: 'terminated' },
+  { label: 'Rollback', value: 'rollback' },
 ];
