971283ca5a23a1a1e56c73181a23cc1390384e18
li <1312513963@qq.com>
2023-04-04 12:42:22 +0800

    update
    Signed-off-by: li <1312513963@qq.com>

--- pkg/server/domain/service/application.go
+++ pkg/server/domain/service/application.go
@@ -32,6 +32,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/rest"
+	"k8s.io/klog/v2"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/yaml"
 
