3b3d1c13ab3629d06dc7e45dca84b02084b11ee5
wuzhongjian <wuzhongjian_yewu@cmss.chinamobile.com>
2023-06-08 10:15:53 +0800

    Feat: add docker-build-velaux makefile
    Signed-off-by: wuzhongjian <wuzhongjian_yewu@cmss.chinamobile.com>

--- makefiles/build.mk
+++ makefiles/build.mk
@@ -1,8 +1,4 @@
 # Build the docker image
 .PHONY: docker-build
-docker-build: docker-build-velaux
-	@$(OK)
-
-.PHONY: docker-build-velaux
-docker-build-velaux:
+docker-build:
 	docker build --build-arg=VERSION=$(VELAUX_VERSION) --build-arg=GITVERSION=$(GIT_COMMIT) -t $(VELAUX_IMAGE) .
