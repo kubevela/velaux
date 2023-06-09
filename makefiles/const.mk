cd717886b85cb2ac9740a63ff85ad3e87ba09e72
wuzhongjian <wuzhongjian_yewu@cmss.chinamobile.com>
2023-06-09 18:04:08 +0800

    Feat: add docker-build-velaux makefile
    Signed-off-by: wuzhongjian <wuzhongjian_yewu@cmss.chinamobile.com>

--- makefiles/const.mk
+++ makefiles/const.mk
@@ -1,9 +1,25 @@
 SHELL := /bin/bash
 
-GOBUILD_ENV = GO111MODULE=on CGO_ENABLED=0
-GOX         = go run github.com/mitchellh/gox
-TARGETS     := darwin/amd64 linux/amd64 windows/amd64
-DIST_DIRS   := find * -type d -exec
+GOLANGCILINT_VERSION ?= 1.49.0
+
+# Get the currently used golang install path (in GOPATH/bin, unless GOBIN is set)
+ifeq (,$(shell go env GOBIN))
+GOBIN=$(shell go env GOPATH)/bin
+else
+GOBIN=$(shell go env GOBIN)
+endif
+
+GLOBAL_GOLANGCILINT := $(shell which golangci-lint)
+GOBIN_GOLANGCILINT:= $(shell which $(GOBIN)/golangci-lint)
+
+# VelaUX version
+VELAUX_VERSION ?= main
+# Repo info
+GIT_COMMIT          ?= git-$(shell git rev-parse --short HEAD)
+GIT_COMMIT_LONG     ?= $(shell git rev-parse HEAD)
+VELAUX_VERSION_KEY    := github.com/kubevela/velaux/version.VelaVersion
+VELAUX_GITVERSION_KEY := github.com/kubevela/velaux/version.GitRevision
+LDFLAGS             ?= "-s -w -X $(VELAUX_VERSION_KEY)=$(VELAUX_VERSION) -X $(VELAUX_GITVERSION_KEY)=$(GIT_COMMIT)"
 
 TIME_LONG	= `date +%Y-%m-%d' '%H:%M:%S`
 TIME_SHORT	= `date +%H:%M:%S`
@@ -20,26 +36,3 @@ WARN	= echo ${TIME} ${YELLOW}[WARN]${CNone}
 ERR		= echo ${TIME} ${RED}[FAIL]${CNone}
 OK		= echo ${TIME} ${GREEN}[ OK ]${CNone}
 FAIL	= (echo ${TIME} ${RED}[FAIL]${CNone} && false)
-
-
-
-# VelaUX version
-VELAUX_VERSION ?= main
-# Repo info
-GIT_COMMIT          ?= git-$(shell git rev-parse --short HEAD)
-GIT_COMMIT_LONG     ?= $(shell git rev-parse HEAD)
-VELAUX_VERSION_KEY    := github.com/kubevela/velaux/version.VelaVersion
-VELAUX_GITVERSION_KEY := github.com/kubevela/velaux/version.GitRevision
-LDFLAGS             ?= "-s -w -X $(VELAUX_VERSION_KEY)=$(VELAUX_VERSION) -X $(VELAUX_GITVERSION_KEY)=$(GIT_COMMIT)"
-
-
-
-# Get the currently used golang install path (in GOPATH/bin, unless GOBIN is set)
-ifeq (,$(shell go env GOBIN))
-GOBIN=$(shell go env GOPATH)/bin
-else
-GOBIN=$(shell go env GOBIN)
-endif
-
-# Image URL to use all building/pushing image targets
-VELAUX_IMAGE      ?= velaux:latest
