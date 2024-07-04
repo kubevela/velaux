73334e7516a77687fe9cdcf7fd1ea5b217982a25
Oana Schipor <36938341+oanasc@users.noreply.github.com>
2024-07-04 16:16:27 +0100

    Fix: Username length in projects users to match users (#904)
    * truncating username in vela_project_user to match vela_user username
    
    Signed-off-by: Oana Schipor <oana.schipor@vortexa.com>
    
    * update fn
    
    Signed-off-by: Oana Schipor <oana.schipor@vortexa.com>
    
    * Remove old k8s version
    
    Signed-off-by: Oana Schipor <oana.schipor@vortexa.com>
    
    ---------
    
    Signed-off-by: Oana Schipor <oana.schipor@vortexa.com>

--- pkg/server/domain/service/authentication.go
+++ pkg/server/domain/service/authentication.go
@@ -449,6 +449,14 @@ func (a *authenticationServiceImpl) GetLoginType(ctx context.Context) (*apisv1.G
 	}, nil
 }
 
+func getUserName(sub string) string {
+	username := strings.ToLower(sub)
+	if len(sub) > datastore.PrimaryKeyMaxLength {
+		return sub[:datastore.PrimaryKeyMaxLength]
+	}
+	return username
+}
+
 func (d *dexHandlerImpl) login(ctx context.Context) (*apisv1.UserBase, error) {
 	var claims struct {
 		Email string `json:"email"`
@@ -492,14 +500,8 @@ func (d *dexHandlerImpl) login(ctx context.Context) (*apisv1.UserBase, error) {
 			klog.Errorf("failed to get the system info %s", err.Error())
 		}
 		user := &model.User{
-			Email: claims.Email,
-			Name: func() string {
-				sub := strings.ToLower(claims.Sub)
-				if len(sub) > datastore.PrimaryKeyMaxLength {
-					return sub[:datastore.PrimaryKeyMaxLength]
-				}
-				return sub
-			}(),
+			Email:         claims.Email,
+			Name:          getUserName(claims.Sub),
 			DexSub:        claims.Sub,
 			Alias:         claims.Name,
 			LastLoginTime: time.Now(),
@@ -514,7 +516,7 @@ func (d *dexHandlerImpl) login(ctx context.Context) (*apisv1.UserBase, error) {
 		if systemInfo != nil {
 			for _, project := range systemInfo.DexUserDefaultProjects {
 				_, err := d.projectService.AddProjectUser(ctx, project.Name, apisv1.AddProjectUserRequest{
-					UserName:  strings.ToLower(claims.Sub),
+					UserName:  getUserName(claims.Sub),
 					UserRoles: project.Roles,
 				})
 				if err != nil {
